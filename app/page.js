1aa0f82e115eb07889d596920dac0061ed51feab
sonam kumari <sonam20@navgurukul.org>
2023-08-10 16:07:39 +0530

    Worked on button

--- app/page.js
+++ app/page.js
@@ -1,95 +1,72 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+import React,{useState} from 'react';
 
-export default function Home() {
+
+const Page = () => {
+// this state for count the button howmany clicked on that particular button.
+    const [red,setRed]=useState(0)
+    const [green,setGreen]=useState(0)
+    const [blue,setBlue]=useState(0)
+    
+    const handleRed = () => {
+        setRed(red+1)    
+    };
+    const handleGreen = () => {
+        setGreen(green+1)    
+    };
+    const handleBlue = () => {
+        setBlue(blue+1)    
+    };
+
+
+    
+// This state i create a change the color of background the box div.
+    let yellow = '#ffc800';
+    const [bgColor, setBgColor] = useState(yellow);
+    
+    const changeColor =()=>{
+      let redbutton = 'red';
+      setBgColor(redbutton);
+    }
+    
+    const changeColorGreen=()=>{
+        let greenbut="green";
+        setBgColor(greenbut);
+        
+    }
+    
+    const changeColorBlue=()=>{
+        let bluebut="blue";
+        setBgColor(bluebut); 
+    }  
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+        <h1>Background Color Change</h1>
+        <div className="box" style={{background: bgColor}}></div>
+        <p className="para">This is color button. If you click on the particular button then color will be change. </p>
+        
+        <div className="button_box1">
+            <h3>Red:{red}</h3>
+            <button className="one" onClick={()=>{handleRed();changeColor();}} >Red</button>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="button_box2">
+            <h3>Green:{green}</h3>
+            <button className="one"  onClick={()=>{handleGreen();changeColorGreen();}} >Green</button>
+        </div>
+ 
+        
+        <div className="button_box3" >
+            <h3>Blue:{blue}</h3>
+            <button className="one"  onClick={()=>{handleBlue();changeColorBlue();}} >Blue</button>
+        </div>
+        
+       
+    </div>
+  );
+};
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+export default Page;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
